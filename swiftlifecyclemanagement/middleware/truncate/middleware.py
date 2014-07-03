61e1fac04e23ff2f56d026e2bbb1f7744d5afa67
seongsoo.cho <nexusz99@a2company.co.kr>
2014-07-04 04:06:51 +0900

    요청 처리 조건을 POST->PUT으로 변경 , object 의 메타데이터의 etag, content-length를 0KB기준으로 지정

--- swiftlifecyclemanagement/middleware/truncate/middleware.py
+++ swiftlifecyclemanagement/middleware/truncate/middleware.py
@@ -1,11 +1,11 @@
 # coding=utf-8
 from copy import copy
 from swift.common.exceptions import DiskFileDeviceUnavailable
-from swift.common.swob import Request, HTTPInsufficientStorage, HTTPAccepted
+from swift.common.swob import Request, HTTPInsufficientStorage, HTTPCreated
 from swift.common.utils import get_logger
 from swift.common.request_helpers import split_and_validate_path
 from swift.obj.diskfile import DiskFileManager
-from common.lifecycle import GLACIER_FLAG_META
+from swiftlifecyclemanagement.common.lifecycle import GLACIER_FLAG_META
 
 
 class TruncateMiddleware(object):
@@ -24,26 +24,25 @@ class TruncateMiddleware(object):
         except DiskFileDeviceUnavailable:
             return HTTPInsufficientStorage(drive=self.device,
                                            request=Request(copy(env)))
-
-        # TODO 기존에 저장되어 있던, S3 관련 meta를 제외한 나머지 metadata 저장
-
         # object flow 상, 임시 데이터를 삭제 후 DiskFileWrite 의 put을 하게 되면,
         # _finalize_put을 호출하게 된다. 이 때, metadata에 설정된 X-Timestamp 값으로
         # object 파일명을 생성하고, 임시 파일로 대체한다.
         # 따라서 별 다른 truncate를 할 필요가 없다.
-        ori_meta = disk_file.get_metadata()
+
+        # TODO 원본 etga와 content-length를 저장할 방법 필요
+        # TODO 원본 Object Metatdata 도 저장해야한다.
+        ori_meta = disk_file.read_metadata()
         metadata = {
             'X-Timestamp': ori_meta['X-Timestamp'],
             'Content-Type': ori_meta['Content-Type'],
-            'ETag': ori_meta['ETag'],
-            'Content-Length': ori_meta['Content_Length'],
+            'ETag': 'd41d8cd98f00b204e9800998ecf8427e',
+            'Content-Length': 0,
             'X-Object-Meta-Glacier': True
         }
-
         with disk_file.create(size=0) as writer:
             writer.put(metadata)
 
-        return HTTPAccepted(request=req)
+        return HTTPCreated(request=req, etag=ori_meta['ETag'])
 
     def get_diskfile(self, device, partition, account, container, obj,
                     **kwargs):
@@ -55,8 +54,7 @@ class TruncateMiddleware(object):
         method = req.method
         self.device, self.partition, self.account, self.container, \
             self.obj = split_and_validate_path(req, 5, 5, True)
-
-        if method == 'POST' and GLACIER_FLAG_META in req.headers:
+        if method == 'PUT' and GLACIER_FLAG_META in req.headers:
             return self.truncate(env)(env, start_response)
 
         return self.app(env, start_response)
