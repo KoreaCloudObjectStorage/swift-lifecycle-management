0e03be88772ddb0298ed8d9c04957b14bb505a95
seongsoo.cho <nexusz99@a2company.co.kr>
2014-07-07 14:52:30 +0900

    시간 검사 오류 수정

--- swiftlifecyclemanagement/daemon/transitor.py
+++ swiftlifecyclemanagement/daemon/transitor.py
@@ -196,7 +196,7 @@ class ObjectTransitor(Daemon):
                 if validation_flg == LIFECYCLE_OK:
                     times = calc_when_actions_do(object_rule, last_modified)
                     actual_expire_time = int(times['Transition'])
-                    if actual_expire_time != int(container):
+                    if actual_expire_time == int(container):
                         self.request_transition(obj)
 
             self.swift.delete_object(self.s3_transition_objects_account,
