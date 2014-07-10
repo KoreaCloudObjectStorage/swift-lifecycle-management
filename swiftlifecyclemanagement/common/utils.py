73ec8523bd98b6e98de8bfa66765d3c2767054dd
seongsoo.cho <nexusz99@a2company.co.kr>
2014-07-10 13:58:35 +0900

    변수명 변

--- swiftlifecyclemanagement/common/utils.py
+++ swiftlifecyclemanagement/common/utils.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def gmt_to_timestamp(gmt_time):
     # convert object's last_modified(UTC TIME) to Unix Timestamp
-    last_modified = datetime.strptime(gmt_time,
+    timestamp = datetime.strptime(gmt_time,
                                       '%a, %d %b %Y %H:%M:%S GMT')
-    last_modified = calendar.timegm(last_modified.utctimetuple())
-    return last_modified
+    timestamp = calendar.timegm(timestamp.utctimetuple())
+    return timestamp
