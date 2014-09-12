1e45d0c21f36acabeda1531075b29e647cab4cd6
seongsoo.cho <nexusz99@a2company.co.kr>
2014-09-12 11:16:47 +0900

    glacier 로 백업된 object 에 대한 정보 내용 변경

--- swiftlifecyclemanagement/common/utils.py
+++ swiftlifecyclemanagement/common/utils.py
@@ -8,3 +8,20 @@ def gmt_to_timestamp(gmt_time):
                                   '%a, %d %b %Y %H:%M:%S GMT')
     timestamp = calendar.timegm(timestamp.utctimetuple())
     return timestamp
+
+
+def make_glacier_hidden_object_name(orig_info, glacier_key):
+    keylength = len(glacier_key)
+    return '%s-%s-%s' % (keylength, glacier_key, orig_info)
+
+
+def get_glacier_key_from_hidden_object(hidden_obj):
+    keylen = hidden_obj.split('-', 1)[0]
+    startpoint = len(keylen)+1
+    return hidden_obj[startpoint:int(keylen)+startpoint]
+
+
+def get_glacier_objname_from_hidden_object(hidden_obj):
+    keylen = hidden_obj.split('-', 1)[0]
+    startlen = len(keylen)+int(keylen)+2
+    return hidden_obj[startlen:]
