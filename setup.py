6f0897978d78f24a8ab9ba6d2ae614f52d1cf9fa
Jongwon Han <addnull@gmail.com>
2014-10-17 02:28:26 +0900

    debian packaging: install daemon and conf

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
-from setuptools import setup, find_packages
-
-import swiftlifecyclemanagement
-
-filter_factory = [
-    'swiftlifecyclemanagement=swiftlifecyclemanagement.middleware.lifecycle.middleware:filter_factory',
-    'swiftobjecttransiton=swiftlifecyclemanagement.middleware.transition.middleware:filter_factory',
-    'swiftobjectrestore=swiftlifecyclemanagement.middleware.restorer.middleware:filter_factory',
-    'swiftobjecttruncate=swiftlifecyclemanagement.middleware.truncate.middleware:filter_factory'
-]
+from setuptools import setup
 
 setup(
-    name='swiftlifecyclemanagement',
-    version=swiftlifecyclemanagement.version,
-    packages=find_packages(),
-    url='http://a2company.co.kr',
-    license='',
-    author='nexusz99',
-    author_email='nexusz99@a2company.co.kr',
-    description='Swift Lifecycle Management Middleware',
-    install_requires=['swift >= 1.13.1', 'boto >= 2.32.1',
-                      'python-dateutil >= 2.2'],
-    entry_points={'paste.filter_factory': filter_factory}
+    setup_requires=['pbr'],
+    pbr=True
 )
